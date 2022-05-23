404897eedc3d4c96fec1f2299b796a3e217095dd
Kohei Suzuki <eagletmt@gmail.com>
2022-05-24 01:42:25 +0900

    bench: Fix username

--- bench/run.sh
+++ bench/run.sh
@@ -14,7 +14,7 @@ mysql_database=m4r
 mysql_table=benchmark_tests
 s3_bucket=eagletmt-test-bucket
 
-mysql -h 127.0.0.1 -u root -p$mysql_password -D $mysql_database --batch < bench/setup.sql
+mysql -h 127.0.0.1 -u $mysql_user -p$mysql_password -D $mysql_database --batch < bench/setup.sql
 
 time cargo run --release -- -h localhost -P 3306 -u $mysql_user -p $mysql_password -D $mysql_database -t $mysql_table -b eagletmt-test-bucket -x m4r/benchmark_tests/ -d -w 1 -C
 time java -jar "$MYS3DUMP_JAR_PATH" -h localhost -P 3306 -u $mysql_user -p $mysql_password -D $mysql_database -t $mysql_table -b eagletmt-test-bucket -x mys3dump/benchmark_tests/ -d -w 1 -C
